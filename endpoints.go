81327e6b6c8c43957b7d2558665d6f9f8561e18c
itschip <onlygamernerd@gmail.com>
2022-03-13 03:32:26 +0100

    feat(pkgs): retreive package endpoint

---
+++ endpoints.go
@@ -0,0 +1,7 @@
+package tebexgo
+
+var (
+	TebexEndpoint = "https://plugin.tebex.io"
+	AllPackagesEndpoint = TebexEndpoint + "/packages"
+	RetrievePackageEndpoint = TebexEndpoint + "/package/"
+)
--- rest_api.go
+++ rest_api.go
@@ -2,6 +2,7 @@ package tebexgo
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -13,6 +14,33 @@ func (s *Session) GetRequest(endpoint string) ([]byte, error) {
 	req := &http.Request{
 		Method: "GET",
 		URL: reqURL,
+		Header: map[string][]string{
+			"X-Tebex-Secret": {s.Secret},
+		},
+	}
+	
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	
+	response, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	
+	return response, nil
+}
+
+func (s *Session) PostRequest(endpoint string, body io.ReadCloser) ([]byte, error) {
+	reqURL, _ := url.Parse(endpoint)
+	
+	req := &http.Request{
+		Method: "GET",
+		URL: reqURL,
+		Body: body,
 		Header: map[string][]string{
 			"X-Tebex-Secret": {s.Secret},
 		},
