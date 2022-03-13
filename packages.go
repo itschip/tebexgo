44f804ec0f7833f63bebb9d984d709f38217bbbd
itschip <onlygamernerd@gmail.com>
2022-03-13 03:32:40 +0100

    add more structs

--- packages.go
+++ packages.go
@@ -1,13 +1,44 @@
 package tebexgo
 
-import "log"
+import (
+	"encoding/json"
+	"log"
+)
 
-func (s *Session) GetAllPackages() (string, error) {
-	resp, err := s.GetRequest("https://plugin.tebex.io/packages")
+func (s *Session) GetAllPackages() (*[]Package, error) {
+	resp, err := s.GetRequest(AllPackagesEndpoint)
 	if err != nil {
 		log.Printf("Failed to fetch packages, Error: %v", err.Error())
-		return "", nil
+		return nil, err
 	}
 	
-	return string(resp), nil
+	var packages []Package
+	
+	// Should abstract this into some function.
+	err = json.Unmarshal(resp, &packages)
+	if err != nil {
+		log.Println("Failed to marshal response")
+		return nil, err
+	}
+	
+	return &packages, nil
+}
+
+func (s *Session) GetPackage(packageId string) (*Package, error)  {
+	resp, err := s.GetRequest(RetrievePackageEndpoint + packageId)
+	if err != nil {
+		log.Printf("Failed to fetch packages, Error: %v", err.Error())
+		return nil, err
+	}
+	
+	var pkg Package
+	
+	// Same here
+	err = json.Unmarshal(resp, &pkg)
+	if err != nil {
+		log.Println("Failed to marshal response")
+		return nil, err
+	}
+	
+	return &pkg, nil
 }
--- structs.go
+++ structs.go
@@ -4,6 +4,8 @@ type Session struct {
 	Secret string
 }
 
+// Server information
+
 type ServerInformation struct {
 	Account
 	Server
@@ -30,4 +32,95 @@ type Server struct {
 	Name string `json:"name"`
 }
 
-type Packages struct {}
+// Packages
+
+type Package struct {
+	Id                int             `json:"id"`
+	Name              string          `json:"name"`
+	Image             string          `json:"image"`
+	Price             int             `json:"price"`
+	ExpiryLength      int             `json:"expiry_length"`
+	ExpiryPeriod      string          `json:"expiry_period"`
+	Type              string          `json:"type"`
+	Category          PackageCategory `json:"category"`
+	GlobalLimit       int             `json:"global_limit"`
+	GlobalLimitPeriod string          `json:"global_limit_period"`
+	UserLimit         int             `json:"user_limit"`
+	Servers           []PackageServer `json:"servers"`
+	RequiredPackages  []interface{}   `json:"required_packages"`
+	RequireAny        bool            `json:"require_any"`
+	CreateGiftcard    bool            `json:"create_giftcard"`
+	ShowUntil         bool            `json:"show_until"`
+	GuiItem           string          `json:"gui_item"`
+	Disabled          bool            `json:"disabled"`
+	DisableQuantity   bool            `json:"disable_quantity"`
+	CustomPrice       bool            `json:"custom_price"`
+	ChooseServer      bool            `json:"choose_server"`
+	LimitExpires      bool            `json:"limit_expires"`
+	InheritCommands   bool            `json:"inherit_commands"`
+	VariableGiftcard  bool            `json:"variable_giftcard"`
+}
+
+type PackageCategory struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type PackageServer struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type UpdatePackageObject struct {
+	Disabled bool
+	Name     string
+	Price    int
+}
+
+// Listing
+
+type Listing struct {
+	Categories []ListingCategory `json:"categories"`
+}
+
+type ListingCategory struct {
+	Id int `json:"id"`
+	Order int `json:"order"`
+	Name string `json:"name"`
+	OnlySubcategories bool `json:"only_subcategories"`
+	Subcategories []interface{} `json:"subcategories"`
+	Packages ListingPackage `json:"packages"`
+}
+
+type ListingPackage struct {
+	Id int `json:"id"`
+	Order int `json:"order"`
+	Name string `json:"name"`
+	Price string `json:"price"`
+	Sale ListingPackageSale `json:"sale"`
+}
+
+type ListingPackageSale struct {
+	Active bool `json:"active"`
+	Discount string `json:"discount"`
+}
+
+// Bans
+
+type Bans struct {
+	Data []BanData `json:"data"`
+}
+
+type BanData struct {
+	Id int `json:"id"`
+	Time string `json:"time"`
+	Ip string `json:"ip"`
+	PaymentEmail string `json:"payment_email"`
+	Reason string `json:"reason"`
+	User BanUser `json:"user"`
+}
+
+type BanUser struct {
+	Ign string `json:"ign"`
+	Uuid string `json:"uuid"`
+}
