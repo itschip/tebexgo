377b1d2491160372db84fd5cbca0f644c044251b
itschip <onlygamernerd@gmail.com>
2022-03-23 02:56:41 +0100

    feat: delete coupon

--- coupons.go
+++ coupons.go
@@ -13,13 +13,12 @@ func (s *Session) GetAllCoupons() (*Coupons, error) {
 		return nil, err
 	}
 
-
 	var coupons Coupons
 	err = internal.UnmarshalResponse(resp, &coupons)
 	if err != nil {
 		return nil, err
 	}
- 
+
 	return &coupons, nil
 }
 
@@ -57,4 +56,14 @@ func (s *Session) CreateCoupon(createObject *CreateCouponObject) (*CouponData, e
 	}
 
 	return &coupon, nil
+}
+
+func (s *Session) DeleteCoupon(couponId string) error {
+	endpoint := fmt.Sprintf("%s/%s", CouponsEndpoint, couponId)
+	resp, err := internal.DeleteRequest(endpoint)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
